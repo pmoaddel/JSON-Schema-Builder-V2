faae6a1377d52078f17cfb9f7a15eabbfb71341e
Payvand Moaddel <payvand.moaddel@genesys.com>
2018-06-19 12:13:50 -0500

    app up and running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'JSON Schema Builder V2';
+  string: tab = 'editor';
+
+  constructor(private location: Location) {}
+
+  setTab(tabName) {
+    this.tab = tabName;
+  }
+
+  ngOnInit() {
+    let hash = this.location.hash;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { RawComponent } from './raw/raw.component';
+import { DesignerComponent } from './designer/designer.component';
+import { SchemaRowComponent } from './schema-row/schema-row.component';
+
+
+const appRoutes: Routes = [
+  { path: 'designer', component: DesignerComponent },
+  { path: 'raw',      component: RawComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RawComponent,
+    DesignerComponent,
+    SchemaRowComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
+    BrowserModule,
+    NgbModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
